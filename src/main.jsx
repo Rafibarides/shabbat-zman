857b6d30eadea96d32e4d1b05eea3c6b1cb1dcb6
Rafibarides <rafibaridesmusic@gmail.com>
2025-01-24 16:34:57 -0500

    done

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,16 @@ import './styles/fonts.css'
 import './index.css'
 import App from './App.jsx'
 
+// Developer Easter Egg
+console.log(`
+✨ Thanks for checking out the console! ✨
+
+🕯️ Developed by Rafi Barides
+🌟 Let's connect! https://www.linkedin.com/in/rafibarides/
+
+🙏 Shabbat Shalom!
+`)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
