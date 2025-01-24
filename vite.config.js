50b3c8ad824a4aa87be6dc08294a7fe824599901
Rafibarides <rafibaridesmusic@gmail.com>
2025-01-24 12:32:20 -0500

    Remove GitHub Actions and update for root GH Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react';
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
-  base: '/shabbat-zman/',
+  base: './',
   plugins: [react()],
   build: {
     outDir: 'dist',
